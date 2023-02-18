e5d1a9c61154f2d32be7e5877445c06146f0872d
Lu Nguyen <nguyentnlu@gmail.com>
2023-02-19 02:45:58 +0700

    merge thinhdev to develop

--- components/BoxShadow.tsx
+++ components/BoxShadow.tsx
@@ -6,14 +6,14 @@ interface Props {
 }
 export const BoxShadow = ({ label, children }: Props) => {
   return (
-    <div className="min-h-screen flex flex-col justify-center py-12 md:px-4 sm:px-6 lg:px-8 sm:w-full">
+    <div className="min-h-screen flex flex-col justify-center py-12 md:px-4 sm:px-6 lg:px-8 sm:w-full login">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           {label}
         </h2>
       </div>
 
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md login-chill">
         <div className="bg-white py-8 px-4 lg:shadow md:shadow sm:rounded-lg sm:px-10">
             {children}
         </div>
