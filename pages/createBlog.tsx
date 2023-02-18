e5857a220a86f0b6bd21ddad0a02b7757edc5fd5
Pham Duc Thinh <thinhpd1@devcamp.edu.vn>
2023-02-18 16:56:47 +0700

    first commit

---
+++ pages/createBlog.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Sidebar } from '@/components/Sidebar';
+
+export default function createBlog() {
+  return (
+    <Sidebar>
+        <button>Create blog</button>
+    </Sidebar>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { Sidebar } from "@/components/Sidebar";
+
+export default function Home() {
+  return (
+    <Sidebar>
+        <button>Home</button>
+    </Sidebar>
+  )
+}
+
