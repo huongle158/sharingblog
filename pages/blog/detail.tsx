42d03ecc9b96003177589bde1385d49eb6747516
Pham Duc Thinh <thinhpd1@devcamp.edu.vn>
2023-03-02 00:20:17 +0700

    setup Page

--- pages/blog/detail.tsx
+++ pages/blog/detail.tsx
@@ -1,4 +1,4 @@
-import { Sidebar } from "@/components/layouts/Sidebar";
+import { Sidebar } from "../../components/layouts/Sidebar";
 import React from "react";
 
 export default function BlogDetail() {
