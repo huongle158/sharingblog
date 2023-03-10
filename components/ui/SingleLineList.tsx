592a94bf1ad541999016acdb7783a072537a51e3
Lu Nguyen <nguyentnlu@gmail.com>
2023-03-11 00:06:31 +0700

    Format file SingleLineList

--- components/ui/SingleLineList.tsx
+++ components/ui/SingleLineList.tsx
@@ -7,79 +7,80 @@ import { useDispatch, useSelector } from "react-redux";
 import { getDetailUser } from "@/store/redux/actions/userAction";
 
 export const SingleLineList = ({ user, title }: any) => {
-    const router = useRouter();
-    const dispatch = useDispatch();
-    const token = Cookies.get("token") || "";
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const token = Cookies.get("token") || "";
 
-    // trạng thái follow
-    const [following,setFollowing] = useState(false)
-    useEffect(() => {
-		const fetchFollower =  async () => {
-			const follower = await followService.getProfileByUsername(token,user.username)
-			if (follower) {
-				setFollowing(follower.profile.following)
-			}
-		}
-		fetchFollower();
-   }, [user,following]);
-    const handleFollowUser = async () => {
-        await followService.followUser(token,user.username)
-			.then((result) => {
-                setFollowing(result.profile.following)
-                console.log(following)
-			})
-			.catch((err) => {
-				message.error(err);
-			})
+  // trạng thái follow
+  const [following, setFollowing] = useState(false)
+  useEffect(() => {
+    const fetchFollower = async () => {
+      const follower = await followService.getProfileByUsername(token, user.username)
+      if (follower) {
+        setFollowing(follower.profile.following)
+      }
     }
-    const handleUnFollowUser = async () => {
-        await followService.unfollowUser(token,user.username)
-			.then((result) => {
-                setFollowing(result.profile.following)
-                console.log("unfollow thành công")
-			})
-			.catch((err) => {
-				message.error(err);
-			})
-    }
-    // ham call get page profiles/username
-    const handleViewProfileUser = async () => {;
-        dispatch(getDetailUser(user))
-            router.push({
-                pathname: '/profiles/[username]',
-                query: {
-                    username: user.username
-                }
-            });
-            
-    }
-    return (
-<List.Item className="flex items-center justify-between">
-        <div className="flex items-center">
-          <a className="cursor-default hover:cursor-pointer flex" onClick={handleViewProfileUser}>
-            
-            <Avatar size={48} src={user.avatar} />
-            <div>
-              <Typography.Text className="ml-2 font-bold">{user.fullname}</Typography.Text><br/>
-              <Typography.Text className="ml-2">
-                @{user.username}
-                {/* {title === null ? '' : title} */}
-              </Typography.Text>
-            </div>
-    </a>
-  </div>
-  <div>
-    {following ? (
-      <Button onClick={handleUnFollowUser}>
-        Đang theo dõi
-      </Button>
-    ) : (
-      <Button type="primary" onClick={handleFollowUser}>
-        Theo dõi
-      </Button>
-    )}
-  </div>
-</List.Item>
+    fetchFollower();
+  }, [user, following]);
+  const handleFollowUser = async () => {
+    await followService.followUser(token, user.username)
+      .then((result) => {
+        setFollowing(result.profile.following)
+        console.log(following)
+      })
+      .catch((err) => {
+        message.error(err);
+      })
+  }
+  const handleUnFollowUser = async () => {
+    await followService.unfollowUser(token, user.username)
+      .then((result) => {
+        setFollowing(result.profile.following)
+        console.log("unfollow thành công")
+      })
+      .catch((err) => {
+        message.error(err);
+      })
+  }
+  // ham call get page profiles/username
+  const handleViewProfileUser = async () => {
+    ;
+    dispatch(getDetailUser(user))
+    router.push({
+      pathname: '/profiles/[username]',
+      query: {
+        username: user.username
+      }
+    });
 
-    )
+  }
+  return (
+    <List.Item className="flex items-center justify-between">
+      <div className="flex items-center">
+        <a className="cursor-default hover:cursor-pointer flex" onClick={handleViewProfileUser}>
+
+          <Avatar size={48} src={user.avatar} />
+          <div>
+            <Typography.Text className="ml-2 font-bold">{user.fullname}</Typography.Text><br />
+            <Typography.Text className="ml-2">
+              @{user.username}
+              {/* {title === null ? '' : title} */}
+            </Typography.Text>
+          </div>
+        </a>
+      </div>
+      <div>
+        {following ? (
+          <Button onClick={handleUnFollowUser}>
+            Đang theo dõi
+          </Button>
+        ) : (
+          <Button type="primary" onClick={handleFollowUser}>
+            Theo dõi
+          </Button>
+        )}
+      </div>
+    </List.Item>
+
+  )
 }
